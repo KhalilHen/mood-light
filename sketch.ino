2bda05721e9869f0535c438cd57b71f041e07a2c
Khalil <khalilhenane@hotmail.com>
2024-08-11 22:31:11 +0200

    Added the keypad and connected it. But haven't yet complete the code

--- sketch.ino
+++ sketch.ino
@@ -1,4 +1,5 @@
 #include "Neopixel.h"
+#include <Keypad.h>
 
 
 
@@ -17,17 +18,32 @@ bool turnOffLED = false;
 int oldButtonValue = LOW; 
 
 
+//keypad
+
+const uint8_t ROWS = 4;
+const uint8_t COLS = 4;
+char keys[ROWS][COLS] = {
+  { '1', '2', '3', 'A' },
+  { '4', '5', '6', 'B' },
+  { '7', '8', '9', 'C' },
+  { '*', '0', '#', 'D' }
+};
 
 
 
 
 
+uint8_t rowPins[ROWS] = { 13, 12, 11, 10 }; // Pins connected to R1, R2, R3, R4
+uint8_t rowPins[ROWS] = { 9, 8, 7, 6 }; // Pins connected to R1, R2, R3, R4
 
+
+Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
 //Photoressitor 
 float lux;
 const float GAMMA = 0.7;
 const float RL10 = 50;
+
 void setup() {
 
   NeoPixel.begin();
@@ -44,12 +60,16 @@ void setup() {
 }
 
 void loop() {
-
+  char key = keypad.getKey();
+ 
 checkButtonValue();
 
 readSliderValue();
 
 if(!turnOffLED) {
+
+
+if(key != NO_KEY) {
 
 adjustColors();
 
@@ -59,6 +79,19 @@ adjustColors();
 //   NeoPixel.setBrightness(10);
   NeoPixel.show();  // send mn updated pixel colors to the NeoPixel hardware.
   delay(2000); 
+} 
+else {
+switch (key)
+
+  case '1': 
+  setColorRed();  
+default: 
+Serial.println("There wen't something wrong");
+break;
+
+}
+
+
 
 } else {
   Serial.println("Light turned off");
